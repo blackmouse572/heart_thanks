51e0dccb928e283e59bd09ed7726107ed6ae270d
Jaden.Nguyen <jaden.nguyen@avepoint.com>
2024-07-24 14:36:30 +0700

    fix: build not compile ts and js file

--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,63 @@ import morganMiddleware from './utils/chalk-morgan.ts'
 
 installGlobals()
 
+const printResponseTime = (time?: number) => {
+	if (!time) return ''
+	if (time < 100) {
+		return chalk.green.bold(time + 'ms')
+	}
+	if (time < 200) {
+		return chalk.yellow.bold(time + 'ms')
+	}
+	if (time < 300) {
+		return chalk.red.bold(time + 'ms')
+	}
+
+	return chalk.red.bold(time + 'ms')
+}
+
+const printStatusCode = (status?: number) => {
+	if (!status) return ''
+	if (status < 300) {
+		return chalk.green.bold(status)
+	}
+
+	if (status < 400) {
+		return chalk.yellow.bold(status)
+	}
+
+	if (status < 500) {
+		return chalk.red.bold(status)
+	}
+}
+const morganMiddleware = morgan(
+	function (tokens, req, res) {
+		return [
+			chalk.bgGreen(chalk.white.bold('[' + tokens.method?.(req, res) + ']')),
+			chalk.visible(
+				printStatusCode(tokens.status?.(req, res) as unknown as number),
+			),
+			chalk.whiteBright.visible(tokens.url?.(req, res)),
+			chalk.bold(
+				printResponseTime(
+					tokens['response-time']?.(req, res) as unknown as number,
+				),
+			),
+			chalk.dim.hex('#f78fb3').bold('@ ' + tokens.date?.(req, res)),
+			chalk.dim.yellow(tokens['remote-addr']?.(req, res)),
+			chalk.dim.hex('#fffa65').bold('from ' + tokens.referrer?.(req, res)),
+		].join(' ')
+	},
+	{
+		skip: (req, res) =>
+			(res.statusCode === 200 &&
+				(req.url?.startsWith('/resources/note-images') ||
+					req.url?.startsWith('/resources/user-images') ||
+					req.url?.startsWith('/resources/healthcheck'))) ||
+			false,
+	},
+)
+
 const MODE = process.env.NODE_ENV ?? 'development'
 const IS_PROD = MODE === 'production'
 const IS_DEV = MODE === 'development'
